6c3d921b0b94d351ae552c6fc23d3b2f6f15cc4b
MauriUrsino <110792984+MauriUrsino@users.noreply.github.com>
2022-11-16 15:09:13 -0300

    Config de la Db

--- api/conf/db.js
+++ api/conf/db.js
@@ -1,1 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
+
+const S = require("sequelize");
+
+const db = new S(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.SERVIDOR,
+    dialect: process.env.DB,
+    logging: false,
+  }
+);
+
+module.exports = db;
