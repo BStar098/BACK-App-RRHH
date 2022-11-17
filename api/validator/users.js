d47d3296048b4f4242d3cd22f599d9d3e3a25790
Rafa Mojica <rafael.mojica27@gmail.com>
2022-11-17 17:32:34 -0500

    Se agregan nuevas validaciones

--- api/validator/users.js
+++ api/validator/users.js
@@ -1,13 +1,15 @@
 const { check } = require("express-validator");
 const { validacionResultado } = require("../middleware/validacionResultado");
 
-const validacionDeCreacionUsuario = [
+const validacionRegistro = [
   check("nombre")
     .exists()
     .withMessage("El campo debe existir")
     .not()
     .isEmpty()
     .withMessage("El campo no puede estar vacio")
+    .contains()
+    .withMessage("El campo debe tener contenido")
     .isAlpha()
     .withMessage("El campo debe tener solo letras"),
   check("apellido")
@@ -16,6 +18,8 @@ const validacionDeCreacionUsuario = [
     .not()
     .isEmpty()
     .withMessage("El campo no puede estar vacio")
+    .contains()
+    .withMessage("El campo debe tener contenido")
     .isAlpha()
     .withMessage("El campo debe tener solo letras"),
   check("domicilio")
@@ -23,13 +27,17 @@ const validacionDeCreacionUsuario = [
     .withMessage("El campo debe existir")
     .not()
     .isEmpty()
-    .withMessage("El campo no puede estar vacio"),
+    .withMessage("El campo no puede estar vacio")
+    .contains()
+    .withMessage("El campo debe tener contenido"),
   check("documento")
     .exists()
     .withMessage("El campo debe existir")
     .not()
     .isEmpty()
     .withMessage("El campo no puede estar vacio")
+    .contains()
+    .withMessage("El campo debe tener contenido")
     .isNumeric()
     .withMessage("El campo debe ser numerico"),
   check("telefono")
@@ -38,6 +46,8 @@ const validacionDeCreacionUsuario = [
     .not()
     .isEmpty()
     .withMessage("El campo no puede estar vacio")
+    .contains()
+    .withMessage("El campo debe tener contenido")
     .isNumeric()
     .withMessage("El campo debe ser numerico"),
   check("fechaDeNacimiento")
@@ -46,21 +56,29 @@ const validacionDeCreacionUsuario = [
     .not()
     .isEmpty()
     .withMessage("El campo no puede estar vacio")
-    .isDate()
-    .withMessage("El campo debe ser una fecha"),
+    .contains()
+    .withMessage("El campo debe tener contenido")
+    .isDate({format: 'YYYY-MM-DD'})
+    .withMessage("El campo debe ser una fecha YYYY-MM-DD"),
   check("tipo"),
   check("residencia")
     .exists()
     .withMessage("El campo debe existir")
     .not()
     .isEmpty()
-    .withMessage("El campo no puede estar vacio"),
+    .withMessage("El campo no puede estar vacio")
+    .contains()
+    .withMessage("El campo debe tener contenido")
+    .isAlpha()
+    .withMessage("El campo debe tener solo letras"),
   check("eMail")
     .exists()
     .withMessage("El campo debe existir")
     .not()
     .isEmpty()
     .withMessage("El campo no puede estar vacio")
+    .contains()
+    .withMessage("El campo debe tener contenido")
     .isEmail()
     .withMessage("El email no es valido"),
   check("contrasena")
@@ -69,6 +87,8 @@ const validacionDeCreacionUsuario = [
     .not()
     .isEmpty()
     .withMessage("El campo no puede estar vacio")
+    .contains()
+    .withMessage("El campo debe tener contenido")
     .isLength({ min: 8 })
     .withMessage("El campo debe tener minimo 8 caracteres")
     .isStrongPassword()
@@ -78,4 +98,32 @@ const validacionDeCreacionUsuario = [
   }
 ];
 
-module.exports = { validacionDeCreacionUsuario };
+const validacionInicioSesion = [
+  check("eMail")
+    .exists()
+    .withMessage("El campo debe existir")
+    .not()
+    .isEmpty()
+    .withMessage("El campo no puede estar vacio")
+    .contains()
+    .withMessage("El campo debe tener contenido")
+    .isEmail()
+    .withMessage("El email no es valido"),
+  check("contrasena")
+    .exists()
+    .withMessage("El campo debe existir")
+    .not()
+    .isEmpty()
+    .withMessage("El campo no puede estar vacio")
+    .contains()
+    .withMessage("El campo debe tener contenido")
+    .isLength({ min: 8 })
+    .withMessage("El campo debe tener minimo 8 caracteres")
+    .isStrongPassword()
+    .withMessage("El campo debe tener 1 letra en mayuscual, 1 letra en minuscula, 1 numero, 1 simbolo"),
+  (req, res, next) => {
+    validacionResultado(req, res, next)
+  }
+];
+
+module.exports = { validacionRegistro, validacionInicioSesion };
