8dc09f922a9d3d05a61e01b41eda48c8a4d0e8cf
Rafa Mojica <rafael.mojica27@gmail.com>
2022-11-17 12:26:13 -0500

    Se crean nuevas validaciones

--- api/validator/users.js
+++ api/validator/users.js
@@ -1,38 +1,81 @@
 const { check } = require("express-validator");
 const { validacionResultado } = require("../middleware/validacionResultado");
 
-const validacionDeCreacion = [
+const validacionDeCreacionUsuario = [
   check("nombre")
     .exists()
     .withMessage("El campo debe existir")
     .not()
     .isEmpty()
-    .withMessage("El campo no puede estar vacio"),
+    .withMessage("El campo no puede estar vacio")
+    .isAlpha()
+    .withMessage("El campo debe tener solo letras"),
   check("apellido")
-    .exists(),
+    .exists()
+    .withMessage("El campo debe existir")
+    .not()
+    .isEmpty()
+    .withMessage("El campo no puede estar vacio")
+    .isAlpha()
+    .withMessage("El campo debe tener solo letras"),
   check("domicilio")
-    .exists(),
+    .exists()
+    .withMessage("El campo debe existir")
+    .not()
+    .isEmpty()
+    .withMessage("El campo no puede estar vacio"),
   check("documento")
-    .exists(),
+    .exists()
+    .withMessage("El campo debe existir")
+    .not()
+    .isEmpty()
+    .withMessage("El campo no puede estar vacio")
+    .isNumeric()
+    .withMessage("El campo debe ser numerico"),
   check("telefono")
     .exists()
     .withMessage("El campo debe existir")
+    .not()
+    .isEmpty()
+    .withMessage("El campo no puede estar vacio")
     .isNumeric()
     .withMessage("El campo debe ser numerico"),
   check("fechaDeNacimiento")
-    .exists(),
-  check("tipo")
-    .exists(),
+    .exists()
+    .withMessage("El campo debe existir")
+    .not()
+    .isEmpty()
+    .withMessage("El campo no puede estar vacio")
+    .isDate()
+    .withMessage("El campo debe ser una fecha"),
+  check("tipo"),
   check("residencia")
-    .exists(),
+    .exists()
+    .withMessage("El campo debe existir")
+    .not()
+    .isEmpty()
+    .withMessage("El campo no puede estar vacio"),
   check("eMail")
     .exists()
     .withMessage("El campo debe existir")
+    .not()
+    .isEmpty()
+    .withMessage("El campo no puede estar vacio")
     .isEmail()
     .withMessage("El email no es valido"),
+  check("contrasena")
+    .exists()
+    .withMessage("El campo debe existir")
+    .not()
+    .isEmpty()
+    .withMessage("El campo no puede estar vacio")
+    .isLength({ min: 8 })
+    .withMessage("El campo debe tener minimo 8 caracteres")
+    .isStrongPassword()
+    .withMessage("El campo debe tener 1 letra en mayuscual, 1 letra en minuscula, 1 numero, 1 simbolo"),
   (req, res, next) => {
     validacionResultado(req, res, next)
   }
 ];
 
-module.exports = { validacionDeCreacion };
+module.exports = { validacionDeCreacionUsuario };
