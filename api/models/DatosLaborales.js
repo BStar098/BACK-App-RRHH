73b8e68be3953bb4cadca9ebf924cc449aa9940a
Rafa Mojica <rafael.mojica27@gmail.com>
2022-11-22 18:25:33 -0500

    Se crea modelo de asistencias y se actualiza

--- api/models/DatosLaborales.js
+++ api/models/DatosLaborales.js
@@ -7,7 +7,7 @@ DatosLaborales.init(
   {
     fechaDeIngreso: { type: S.DATEONLY },
     puesto: { type: S.STRING },
-    diasLaborales: { type: S.INTEGER },
+    diasLaborales: { type: S.STRING },
     horarioLaboral: { type: S.STRING },
     observaciones: { type: S.TEXT },
     jerarquia: { type: S.INTEGER },
@@ -21,31 +21,22 @@ DatosLaborales.init(
 //HOOKS
 DatosLaborales.addHook("beforeCreate", (datosLaborales, options) => {
   const datos = datosLaborales;
-  if (datos.puesto === "empleado" || "Empleado") {
-    datos.jerarquia = 8;
-  }
 
-  if (datos.puesto === "coordinador regional") {
-    datos.jerarquia = 7;
-  }
-  if (datos.puesto === "jefe pais" || "Jefe Pais") {
-    datos.jerarquia = 6;
-  }
-  if (datos.puesto === "jefe regional" || "Jefe Regional") {
-    datos.jerarquia = 5;
-  }
-  if (datos.puesto === "gerente pais" || "Gerente Pais") {
-    datos.jerarquia = 4;
-  }
-  if (datos.puesto === "rrhh pais" || "RRHH Pais") {
-    datos.jerarquia = 3;
-  }
-  if (datos.puesto === "gerente regional" || "Gerente Regional") {
-    datos.jerarquia = 2;
-  }
-  if (datos.puesto === "gerente general" || "Gerente General") {
-    datos.jerarquia = 1;
-  }
+  if (datos.puesto === "empleado" || "Empleado") return datos.jerarquia = 8
+
+  if (datos.puesto === "coordinador regional") return datos.jerarquia = 7;
+
+  if (datos.puesto === "jefe pais" || "Jefe Pais") return datos.jerarquia = 6;
+
+  if (datos.puesto === "jefe regional" || "Jefe Regional") return datos.jerarquia = 5;
+
+  if (datos.puesto === "gerente pais" || "Gerente Pais") return datos.jerarquia = 4;
+
+  if (datos.puesto === "rrhh pais" || "RRHH Pais") return datos.jerarquia = 3;
+
+  if (datos.puesto === "gerente regional" || "Gerente Regional") return datos.jerarquia = 2;
+
+  if (datos.puesto === "gerente general" || "Gerente General") return datos.jerarquia = 1;
 });
 
 module.exports = DatosLaborales;
