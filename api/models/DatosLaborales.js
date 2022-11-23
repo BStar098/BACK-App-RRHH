6e5574c6374834acfc04a250058262eb58da18f3
MauriUrsino <110792984+MauriUrsino@users.noreply.github.com>
2022-11-22 22:26:58 -0300

    Arreglos del bug del hook
    tambien agregue un get() para que todo lo q envies en puesto se ponga en minuscula

--- api/models/DatosLaborales.js
+++ api/models/DatosLaborales.js
@@ -6,7 +6,13 @@ class DatosLaborales extends S.Model {}
 DatosLaborales.init(
   {
     fechaDeIngreso: { type: S.DATEONLY },
-    puesto: { type: S.STRING },
+    puesto: {
+      type: S.STRING,
+      get() {
+        const data = this.getDataValue("puesto");
+        return data.toLowerCase();
+      },
+    },
     diasLaborales: { type: S.STRING },
     horarioLaboral: { type: S.STRING },
     observaciones: { type: S.TEXT },
@@ -20,23 +26,36 @@ DatosLaborales.init(
 
 //HOOKS
 DatosLaborales.addHook("beforeCreate", (datosLaborales, options) => {
-  const datos = datosLaborales;
-
-  if (datos.puesto === "empleado" || "Empleado") return datos.jerarquia = 8
-
-  if (datos.puesto === "coordinador regional") return datos.jerarquia = 7;
-
-  if (datos.puesto === "jefe pais" || "Jefe Pais") return datos.jerarquia = 6;
-
-  if (datos.puesto === "jefe regional" || "Jefe Regional") return datos.jerarquia = 5;
-
-  if (datos.puesto === "gerente pais" || "Gerente Pais") return datos.jerarquia = 4;
-
-  if (datos.puesto === "rrhh pais" || "RRHH Pais") return datos.jerarquia = 3;
-
-  if (datos.puesto === "gerente regional" || "Gerente Regional") return datos.jerarquia = 2;
-
-  if (datos.puesto === "gerente general" || "Gerente General") return datos.jerarquia = 1;
+  const datos = datosLaborales.puesto;
+  switch (datos) {
+    case "empleado":
+      datosLaborales.jerarquia = 8;
+      break;
+    case "coordinador regional":
+      datosLaborales.jerarquia = 7;
+      break;
+    case "jefe pais":
+      datosLaborales.jerarquia = 6;
+      break;
+    case "jefe regional":
+      datosLaborales.jerarquia = 5;
+      break;
+    case "gerente pais":
+      datosLaborales.jerarquia = 4;
+      break;
+    case "rrhh pais":
+      datosLaborales.jerarquia = 3;
+      break;
+    case "gerente regioanal":
+      datosLaborales.jerarquia = 2;
+      break;
+    case "gerente general":
+      datosLaborales.jerarquia = 1;
+      break;
+    default:
+      datosLaborales.jerarquia = 0;
+      break;
+  }
 });
 
 module.exports = DatosLaborales;
