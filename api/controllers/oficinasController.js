bde3cce7500d3f594f9be7864b8a415a9906df6d
Rafa Mojica <rafael.mojica27@gmail.com>
2022-11-23 11:34:39 -0500

    Se actualiza ruta de oficinas

--- api/controllers/oficinasController.js
+++ api/controllers/oficinasController.js
@@ -3,13 +3,13 @@ const { Oficina, Usuarios, Equipo } = require("../models");
 const crearOficina = async (req, res) => {
   try {
     const { eMail } = req.body;
-    const equipo = await Usuarios.findOne({ where: { eMail }, include: {model: Equipo } });
-    if (!equipo) throw "Usuario no registrado";
+    const usuario = await Usuarios.findOne({ where: { eMail }, include: {model: Equipo } });
+    if (!usuario) throw "Usuario no registrado";
 
     const oficina = await Oficina.create(req.body);
     if (!oficina) throw "Dataos oficina no existe";
 
-    oficina.addEquipo(equipo.equipo);
+    oficina.addEquipo(usuario.equipo);
     res.status(201).send(oficina);
   } catch (error) {
     res.status(400).send(error);
