2a696d2d4fd1b83a1e49f518e0023d96e3babce8
Rafa Mojica <rafael.mojica27@gmail.com>
2022-11-23 11:35:23 -0500

    Se agregan validaciones

--- api/routes/equipos.js
+++ api/routes/equipos.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { crearEquipo } = require("../controllers/equiposController");
+const { crearEquipo, trearEquipoPais } = require("../controllers/equiposController");
 const routesEqipos = express();
 
 //Crear un equipo
-routesEqipos.post("/", crearEquipo);                         //ESTA RUTA ESTA BIEN 
+routesEqipos.post("/", crearEquipo);
 
+//Traer todo el equipo de un pais
+routesEqipos.get("/", trearEquipoPais);
 
 
 module.exports = routesEqipos;
