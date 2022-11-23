60f2b358c742b70aeff6d9b78ce01c645b78fa53
Bstar098 <luceropintos@abc.gob.ar>
2022-11-23 00:03:29 -0300

    Listos los controllers y rutas de asistencias de usuario

---
+++ api/controllers/asistenciasControllers.js
@@ -0,0 +1,24 @@
+const { Asistencia, Usuarios } = require("../models");
+
+const crearAsistencia = async (req, res) => {
+  const { usuarioId, datosAsistencia } = req.body;
+  try {
+    const asistencia = await Asistencia.create(datosAsistencia);
+    asistencia.setUsuario(usuarioId);
+    res.send(asistencia)
+  } catch (error) {
+    throw new Error("No se ha podido crear la asistencia");
+  }
+};
+
+const historialDeAsistencias = async (req, res) => {
+  try {
+    const historial = await Asistencia.findAll({ where: req.body });
+    console.log(historial)
+    res.send(historial)
+  } catch (error) {
+    throw new Error("No se ha podido retornar el historial de asistencias");
+  }
+};
+
+module.exports = { crearAsistencia, historialDeAsistencias };
